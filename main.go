fa8130c6d726a463e5b326e07e08d4b06c227490
Alon Ben-Horin <alon.benhorin@cyberark.com>
2025-09-08 13:48:26 +0300

    DB connection

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	host := "db.example.com"
+	port := 5432
+	user := "admin"
+	password := "supersecret123"
+	dbname := "production_db"
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
 }
